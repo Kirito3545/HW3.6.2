78b8479f39861b40d353243ae645b3ebac324464
Mikhail Dubrovskiy <dmaz68@mail.ru>
2023-05-04 23:54:26 +0300

    Initial commit

--- Counter.h
+++ Counter.h
@@ -1,1 +1,12 @@
 #pragma once
+class Counter 
+{
+private:
+    int counterResult_ = 0;
+
+public:
+    Counter(int initialInitValue);
+    int getCounter();
+    int counterPlus();
+    int counterMinus();
+};
